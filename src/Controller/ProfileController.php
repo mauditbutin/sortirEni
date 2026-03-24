5b3d620105153f4185ad9b72b012eaab66336109
SymfonyProject <rkhonyakov@gmail.com>
2026-03-24 16:58:58 +0100

    Create : 1. Controller de Profil 2. show.html.twig

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProfileController extends AbstractController
 {
     #[Route('/account/{id}', name: 'app_profile')]
-    public function index(): Response
+    public function show(int $id, UserRepository $userRepository ): Response
     {
-        return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+        $user = $userRepository->find($id);
+        // if user not founded
+        if (!$user) {
+            throw $this->createNotFoundException('User not found !!!');
+        }
+        // render - make back html page
+        return $this->render('profile/show.html.twig', [
+            'user' => $user,
         ]);
     }
 }
