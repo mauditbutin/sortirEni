20e00022d4a6e7ea559656ee14b970845db41fe7
Maud <maudbutin@gmail.com>
2026-03-25 21:52:51 +0100

    Create Hike : ajout d'un formulaire de création de lieux associé dans une modale

--- src/Form/HikeCreateType.php
+++ src/Form/HikeCreateType.php
@@ -26,66 +26,35 @@ class HikeCreateType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class,
-                ['label' => 'Titre'])
-            ->add('description', TextareaType::class, [
-                'label' => 'Description et infos'
-            ])
-            //----------------------------------------------
-            // A mettre sous forme heures minutes -> minutes
-            ->add('duration', IntegerType::class, [
-                'label' => 'Durée (en minutes)',
-            ])
-            //----------------------------------------------
-            ->add('dateEvent', DateTimeType::class, [
-                'label' => 'Date et heure de l\'évènement'
-            ])
-            ->add('dateSubscription', DateType::class, [
-                'label' => 'Date limite d\'inscription'
-            ])
-            ->add('nbMaxSubscription', IntegerType::class, [
-                'label' => 'Nombre de places'
-            ])
-            ->add('picture', FileType::class, [
-                'label' => 'Ajouter une photo',
-                'mapped' => false
-            ])
-//            ->add('status', EntityType::class, [
-//                'class' => Status::class,
-//                'choice_label' => 'id',
-//            ])
+            ->add('name', TextType::class)
+            ->add('dateEvent', DateTimeType::class)
+            ->add('dateSubscription', DateType::class)
+            ->add('nbMaxSubscription', IntegerType::class)
             ->add('difficulty', EntityType::class, [
                 'class' => Difficulty::class,
-                'label' => 'Difficulté',
                 'placeholder' => 'Sélectionnez un niveau',
                 'choice_label' => 'label',
             ])
+            ->add('duration', IntegerType::class)
+            ->add('description', TextareaType::class)
+            ->add('picture', FileType::class, [
+                'mapped' => false
+            ])
             ->add('location', EntityType::class, [
                 'class' => Location::class,
-                'label' => 'Lieu de la randonnée',
                 'placeholder' => 'Sélectionnez un lieu',
                 'choice_label' => 'name',
             ])
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
-                'label' => 'Campus',
                 'choice_label' => 'name',
             ])
-//            ->add('planner', EntityType::class, [
-//                'class' => User::class,
-//                'choice_label' => 'id',
-//            ])
-//            ->add('participants', EntityType::class, [
-//                'class' => User::class,
-//                'choice_label' => 'id',
-//                'multiple' => true,
-//            ])
-        ->add('create', SubmitType::class, [
-            'label' => 'Créer',
-        ])
-        ->add('publish', SubmitType::class, [
-            'label' => 'Publier',
-        ]);
+            ->add('create', SubmitType::class, [
+                'label' => 'Créer',
+            ])
+            ->add('publish', SubmitType::class, [
+                'label' => 'Publier',
+            ]);
 
     }
 
