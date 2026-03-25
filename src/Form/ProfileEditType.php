317d941677a5aa40bfe18c3a6a8be796b53c1057
SymfonyProject <rkhonyakov@gmail.com>
2026-03-25 16:07:04 +0100

    1.Create ProfileEdit form 2. Edit ProfileController 3. Create page pour edit profile

---
+++ src/Form/ProfileEditType.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Hike;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ProfileEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Name',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Name is cannot be empty']),
+                    new Length(['min' => 2, 'max' => 50]),
+                ],
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Lastname',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Lastname is cannot be empty']),
+                    new Length(['min' => 2, 'max' => 50]),
+                ],
+            ])
+            ->add('phoneNumber', TelType::class, [
+                'label' => 'Phone',
+                'required' => false,
+                'constraints' => [
+                    new Length(['max' => 50]),
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'required' => true,
+                'constraints' => [
+                    new NotBlank(['message' => 'Email is cannot be empty']),
+                    new Length(['max' => 100]),
+                ],
+            ])
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'id',
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'label' => 'New Password',
+                'mapped' => false,
+                'required' => false,
+                'attr' => [
+                    'placeholder' => 'Leave blank to keep current password',
+                ],
+                'constraints' => [
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Password must be at least 8 characters long',
+                        'max' => 255,
+                    ]),
+                ],
+            ]);
+
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
