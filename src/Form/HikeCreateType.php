52a262fc71a7d5778620e44a21363796239ed6b8
Maud <maudbutin@gmail.com>
2026-03-27 11:17:39 +0100

    Ajout messages toast

--- src/Form/HikeCreateType.php
+++ src/Form/HikeCreateType.php
@@ -51,7 +51,7 @@ class HikeCreateType extends AbstractType
                 'class' => Location::class,
                 'placeholder' => 'Sélectionnez un lieu',
                 'choice_label' => 'name',
-                'choices' => []
+//                'choices' => []
             ])
             ->add('campus', EntityType::class, [
                 'class' => Campus::class,
