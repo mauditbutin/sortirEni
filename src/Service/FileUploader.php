355ed5950cd1e8f941ca29f8d2575db4b9cdfc09
SymfonyProject <rkhonyakov@gmail.com>
2026-04-02 11:08:27 +0200

    1. Fix the bug UPLOAD THE IMAGE (FINAL)

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -2,25 +2,44 @@
 
 namespace App\Service;
 
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class FileUploader
 {
+
+    public function __construct(
+        private SluggerInterface $slugger,
+    ) {
+
+    }
 
     public function uploadFile(
         UploadedFile $file,
         string $directory,
         string $name='',
-    )
+    ): string
     {
-        $newFileName = ($name ? str_replace(' ', '-', $name) . '-' : '') . uniqid() . '.' . $file->guessExtension();
-        $file->move($directory, $newFileName);
+        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+        $safeFilename = $this->slugger->slug($name ?: $originalFilename);
+        $newFileName = ($safeFilename . '-' . uniqid() . '.' . $file->guessExtension());
+
+        try {
+            $file->move($directory, $newFileName);
+        } catch (FileException $e) {
+            throw new \RuntimeException('Could not upload the file');
+        }
+
         return $newFileName;
     }
 
-    public function deleteFile(string $fileName, string $directory)
+    public function deleteFile(string $fileName, string $directory): void
     {
-        return unlink($directory . DIRECTORY_SEPARATOR . $fileName);
+        $path = $directory . DIRECTORY_SEPARATOR . $fileName;
+        if (is_file($path)) {
+            unlink($path);
+        }
     }
 
 }
